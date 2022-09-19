5f08f3e572c47467b776e05c4f2f6d4f15a9257c
Bladesheng <keadr23@gmail.com>
2022-09-19 12:01:09 +0200

    Create basic form for input

--- src/components/CvInput.tsx
+++ src/components/CvInput.tsx
@@ -5,9 +5,49 @@ class CvInput extends React.Component {
   render() {
     return (
       <div className="main">
-        <div className="cvInput">
-          <p>input elements here</p>
-        </div>
+        <form className="cvInput">
+          <fieldset className="personalInfo">
+            <legend>Personal Information</legend>
+
+            <label htmlFor="firstName">First name</label>
+            <input type="text" id="firstName"></input>
+
+            <label htmlFor="lastName">Last name</label>
+            <input type="text" id="lastName"></input>
+
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title"></input>
+
+            <label htmlFor="adress">Adress</label>
+            <input type="text" id="adress"></input>
+
+            <label htmlFor="phoneNumber">Phone number</label>
+            <input type="tel" id="phoneNumber"></input>
+
+            <label htmlFor="email">Email</label>
+            <input type="tel" id="email"></input>
+
+            <label htmlFor="description">Description</label>
+            <textarea id="description"></textarea>
+          </fieldset>
+
+          <fieldset className="education">
+            <legend>Education</legend>
+
+            <button>Add</button>
+          </fieldset>
+
+          <fieldset className="experience">
+            <legend>Experience</legend>
+
+            <button>Add</button>
+          </fieldset>
+
+          <button>Generate PDF</button>
+          <button>Load Example</button>
+          <button>Reset Form</button>
+        </form>
+
         <CvOutput></CvOutput>
       </div>
     );
