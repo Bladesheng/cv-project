4affa74a7baf734e3b531a1adfc4e87858d8f713
Bladesheng <keadr23@gmail.com>
2022-09-23 12:18:58 +0200

    Refactor Footer into functional component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <p>
-          Made by <a href="https://github.com/Bladesheng">Bladesheng</a>
-        </p>
-      </footer>
-    );
-  }
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made by <a href="https://github.com/Bladesheng">Bladesheng</a>
+      </p>
+    </footer>
+  );
 }
-
-export default Footer;
