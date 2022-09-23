0ac4c241da891c0bd2c89c217c068409016adf93
Bladesheng <keadr23@gmail.com>
2022-09-23 12:29:39 +0200

    Refactor App into functional component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import CvInput from "./CvInput";
 import Footer from "./Footer";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <CvInput></CvInput>
-        <Footer></Footer>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="app">
+      <CvInput></CvInput>
+      <Footer></Footer>
+    </div>
+  );
 }
-
-export default App;
